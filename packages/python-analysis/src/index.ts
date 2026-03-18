5ec577577e20ffce9ba5206407985a8909167a75
dnwpark <dnwpark@protonmail.com>
2026-03-17 18:20:32 -0700

    Simplify tests.

--- packages/python-analysis/src/index.ts
+++ packages/python-analysis/src/index.ts
@@ -96,6 +96,7 @@ export { selectPython, selectPythonVersion } from './manifest/python-selector';
 // =============================================================================
 
 export { PythonAnalysisError } from './util/error';
+export { parseConfig } from './util/config';
 
 // =============================================================================
 // Schemas (runtime validation)
@@ -198,6 +199,7 @@ export {
 // =============================================================================
 
 export {
+  parsePythonVersionFile,
   parseUvPythonRequest,
   serializePythonRequest,
   writePythonVersionFile,
